ab7cf53743d235bc04125ee742fc2300bf463ede
Lionel Beato <lbeato@techtalentsouth.com>
2020-02-18 15:46:19 -0500

    typescript website

---
+++ script.ts
@@ -0,0 +1,26 @@
+class Animal {
+    name:string
+    color:string
+    isAnAnimal(){
+        return true
+
+    }
+
+}
+
+class Duck extends Animal {
+    wings:true
+    quack (){
+        return 'Duck goes quack')
+    }
+}
+
+class Chicken extends Animal {
+    return 'this duck laid an egg'
+
+}
+
+class Chicken e
+const daffy = new Duck()
+
+console.log(daffy.quack())
